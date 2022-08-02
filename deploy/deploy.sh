ad5b89bf30ad9ade86cdd6d0d6938c5af4933b83
David <davfreismuth@gmail.com>
2022-08-02 16:07:51 +0000

    Added apache server to the deployment.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,8 @@
 REPO_ROOT=../
 
-docker build $REPO_ROOT/deploy/docker/apache/Dockerfile
+# Copy the web folder into the build context.
+cp -r $REPO_ROOT/web/ $REPO_ROOT/deploy/docker/apache/web/
+# Build and run the web_server
+docker container rm -f web_server
+docker build -t phidippus_web_server $REPO_ROOT/deploy/docker/apache/
+docker run -dit --name web_server -p 80:80 phidippus_web_server
